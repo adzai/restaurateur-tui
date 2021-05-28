3780330784830eab2a6c33b807b02a28056c10ca
Adam Zaiter <zaiter.adam7@gmail.com>
2021-05-28 18:49:02 +0200

    remove comments

--- restaurateur-tui.py
+++ restaurateur-tui.py
@@ -3,9 +3,6 @@ import curses
 import os
 
 
-# TODO: adjust data from JSON to be more relevant
-# TODO: Handle truncating of text in menus or r for reload
-# TODO: add some vim key bindings
 if __name__ == "__main__":
     os.environ.setdefault('ESCDELAY', '25')
     curses.wrapper(main_loop)
